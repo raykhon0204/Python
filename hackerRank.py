08595c408c9ae48ee230c21f7a5e5bdd9dd38f0f
raykhon0204 <55024131+raykhon0204@users.noreply.github.com>
2020-07-06 10:36:49 -0400

    changes before functions

---
+++ hackerRank.py
@@ -0,0 +1,16 @@
+list1 = []
+
+list1.append(5)
+
+list1.append(10)
+
+list1.append(6)
+print(list1)
+list1.remove(6)
+list1.append(9)
+list1.append(1)
+list1.sort()
+print(list1)
+list1.pop(3)
+list1.reverse()
+print(list1)
--- while_in_list_dict.py
+++ while_in_list_dict.py
@@ -23,3 +23,69 @@ print(pets)
 while 'cat' in pets:
     pets.remove('cat')
 print(pets)
+
+#filling a dictionary with User Input
+responses = {}
+#set a flag to indicate that polling is active
+polling_active = True
+while polling_active:
+    #prompt for the person's name and response
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
+
+    #store the response in the dictionary:
+    responses[name] = response
+
+    #find out if anyoune else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes / no) ")
+    if repeat == 'no':
+        polling_active = False
+    
+    #polling is complete, show the results
+    print("\n---- Poll Results ----")
+    for name, response in responses.items():
+        print(name + "would you like to clime " +  response + ".")
+    
+    #try it yourself
+    #Deli
+    sandwitch_orders = ['veggie', 'turkey', 'salmon', 'tuna']
+    finished_sandwitches = []
+    while sandwitch_orders:
+        sandwitch = sandwitch_orders.pop()
+        print("I made your " + sandwitch.title())
+        finished_sandwitches.append(sandwitch)
+    print("\nThe following sandwitches have been made today:")
+    for sand in finished_sandwitches:
+        print(sand.title())
+   
+    #NO Pastarami
+    sandwitch_orders = ['veggie', 'turkey', 'salmon', 'tuna']
+    sandwitch_orders.append('pastarami')
+    sandwitch_orders.append('pastarami')
+    sandwitch_orders.append('pastarami')
+    print("Sorry, we ran out of pastarami...")
+    while 'pastarami' in sandwitch_orders:
+        sandwitch_orders.pop()
+    
+    print(sandwitch_orders)
+
+    #Dream vacation
+    places = {}
+    active_poll = True
+    while active_poll:
+        name = input("\nWhat is your name? ")
+        place = input("\nIf you could visit one place in the world, where would you go?")
+
+        places[name] = place
+
+        repeat = input("\nWho else wants to the poll? (Answer y/n)")
+        if repeat == 'n':
+            active_poll = False
+        print("This is the Poll Results: ")
+        for name, place in places.items():
+            print(name + " would like to go to " + place + ".")
+
+
+
+
+    
