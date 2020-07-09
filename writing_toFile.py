5eba99f90269c50f5f7c61e3e0d77cc5e859ffc0
raykhon0204 <55024131+raykhon0204@users.noreply.github.com>
2020-07-09 12:14:42 -0400

    files

---
+++ writing_toFile.py
@@ -0,0 +1,23 @@
+#writing to an empty file
+#the second argument in open tell python that we want to open the file in write mode
+#read mode ('r'), write mode ('w'), append mode ('a')
+# read and write to the file ('r+')
+#If you omit the mode argument, Python opens the file in read-only mode by default
+#be careful opening a file in write mode ('w') because if the file does exist, 
+# Python will erase the file before returning the file object.
+fileName = 'programming.txt'
+#with open(fileName, 'w') as file_obj:
+    #file_obj.write("I love programming.")
+
+with open(fileName, 'a') as file_obj1:
+    file_obj1.write("I also love finding meaning in large datasets. \n")
+    file_obj1.write("I love creating apps that can run in a browser. \n") 
+
+prompt = "What is you name?\n"
+prompt += "If there no more guests, please enter 'q' to quit.\n"
+guest = ''
+while guest != 'q':
+    guest = input(prompt)
+    with open('guests.txt', 'a') as guest_file:
+        guest_file.write(guest + "\n")
+        print("Hello, " + guest + ", welcome to the party!")
